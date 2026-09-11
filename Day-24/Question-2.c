069f8ab5c0fab9c68855899b444652c210b71765
naishabatra <batranaisha69@gmail.com>
2026-09-11 22:01:53 +0530

    Add pattern printing program in Question-2.c
    Implement a program to print a number pattern.

---
+++ Day-24/Question-2.c
@@ -0,0 +1,17 @@
+//Q48: Write a program to print the following pattern:
+#include <stdio.h>
+
+void main()
+{
+    int i, j;
+
+    for(i = 1; i <= 5; i++)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+}
