4cbe9ef68756b4a64a628fa5efe31b828610e4f6
naishabatra <batranaisha69@gmail.com>
2026-09-15 22:56:32 +0530

    Add matrix sum calculation program
    Implement a program to calculate the sum of all elements in a matrix.

---
+++ Day-36/Question-2.c
@@ -0,0 +1,20 @@
+//Q72: Find the sum of all elements in a matrix.
+#include <stdio.h>
+
+void main()
+{
+    int a[10][10], r, c, i, j, sum = 0;
+
+    scanf("%d %d", &r, &c);
+
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            scanf("%d", &a[i][j]);
+            sum = sum + a[i][j];
+        }
+    }
+
+    printf("%d", sum);
+}
