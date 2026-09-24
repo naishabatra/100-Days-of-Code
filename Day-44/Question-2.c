92fe7680a867eb3f8711ed2f18c49efcee16c8fa
naishabatra <batranaisha69@gmail.com>
2026-09-25 01:07:10 +0530

    Add string space-to-hyphen replacement program
    Implemented a program to replace spaces with hyphens in a string.

---
+++ Day-44/Question-2.c
@@ -0,0 +1,19 @@
+//Q88: Replace spaces with hyphens in a string.
+
+#include <stdio.h>
+
+void main()
+{
+    char s[100];
+    int i;
+
+    gets(s);
+
+    for(i = 0; s[i] != '\0'; i++)
+    {
+        if(s[i] == ' ')
+            s[i] = '-';
+    }
+
+    printf("%s", s);
+}
